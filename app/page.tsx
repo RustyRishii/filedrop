293a3102abe06bf4698c83d105cb87b21bcc22b3
RustyRishii <rishikeshbidkar04@gmail.com>
2025-01-12 21:45:25 +0530

    Commit test 6

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       Welcome to file drop
-      <button>upoad a file please</button>
+      <button>upoad a file hahaha</button>
     </div>
   );
 }
