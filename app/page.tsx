011a14c25fb16bf16fc8a2c793dd718d29ba14ea
RustyRishii <rishikeshbidkar04@gmail.com>
2025-01-13 23:52:31 +0530

    Adding auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-// import Image from "next/image";
+// import { createClient } from "@supabase/supabase-js";
+import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div>
-      Welcome to file drop
-      <button>upoad a file hahaha</button>
+      <div className="snap-start items-center justify-center h-screen flex-col flex">
+        <h1 className="text-4xl">Welcome to file drop</h1>
+        {/* <input type="file" name="file"></input> */}
+        <button  className="flex items-center border border-white rounded-full p-2" >
+          <Image src="google.svg" alt="Google logo" height={35} width={35} />
+          <p className="">Login with google</p>
+        </button>
+      </div>
     </div>
   );
 }
